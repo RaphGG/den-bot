94442cc04e124ce2e77c26535be5fe9c0ab61830
Raphael <raphg@knights.ucf.edu>
2020-01-06 23:33:29 -0500

    Role Changes

--- mybot.js
+++ mybot.js
@@ -48,15 +48,37 @@ client.on("message", (message) => {
 
   if (command === "den")
   {
-    if (args.length == 0 || (args[0] > 93 || args[0] < 1))
+    if (args.length == 0 || (args[0].valueOf() > 93 || args[0].valueOf() < 1))
     {
       message.channel.send("Please enter a den number within the range of 1-93.");
-      message.delete();
+      //message.delete();
       return;
     }
 
-    message.reply(`Den ${args[0]} has the following pokemon: `, {files: [`./dens/den${args[0]}.png`]});
-    message.delete();
+    message.reply(`Den ${args[0]} has the following Pokémon: `, {files: [`./dens/den${args[0]}.png`]});
+    //message.delete();
+  }
+
+  else if (command === "ping")
+  {
+    let adminRole = message.guild.roles.find(role => role.name == "Admin");
+    if (!message.member.roles.has(adminRole.id))
+    {
+      console.log("Admin?");
+      return;
+    }
+
+    if (args.length == 0)
+    {
+      message.channel.send("Please enter a role to ping.");
+      return;
+    }
+
+    let myRole = message.guild.roles.find(role => role.name == args[0]);
+    myRole.setMentionable(true, "Role to be pinged.");
+    message.channel.send(`${myRole}`).then(() => {
+      myRole.setMentionable(false, "Role has been Pinged.")});
+    return;
   }
 });
 
