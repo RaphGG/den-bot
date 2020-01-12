9e23fe89c64f6282b35656c1ba4f607375609b60
Raphael <raphg@knights.ucf.edu>
2020-01-12 02:14:18 -0500

    Catch Rate Finalization + Control Structure Rough Implementation

--- mybot.js
+++ mybot.js
@@ -20,6 +20,32 @@ var ingamePkmn = pokemon.filter(x => {
   return availablepokemon.includes(x.Name);
 });
 
+ingamePkmn.forEach(pkmn => {
+  let gf = "(Galarian Form)";
+  let nameLength = pkmn.Name.length;
+
+  if (pkmn.Name.endsWith(gf))
+    pkmn.Name = pkmn.Name.slice(0, (nameLength - gf.length-1));
+
+});
+
+var ballNames = new Array();
+
+balls.forEach(ball => {
+  let ballNameLength = ball.name.length;
+  let ballNameFull = ball.name.replace(" ", "").toLowerCase();
+  let ballNameAbbr = ball.name.substring(0, (ballNameLength - "Ball".length-1)).toLowerCase();
+
+  ballNames.push(ball.name);
+  ballNames.push(ballNameFull);
+  ballNames.push(ballNameAbbr);
+});
+
+var pkmnNotFound = "The Pokémon requested was either not found or not available in current dens.";
+var ballNotFound = "The Poké-Ball requested was not found.";
+var notFound = "The command or argument requested was not found.";
+var gigaKeywords = ["g", "-g", "g-", "giga", "gigantamax", "gmax"];
+
 const baseStatCalc = (stat, iv, level) => {
   let x = ( ( (2 * stat) + iv ) * level) / 100;
   return Math.floor(x) + level + 10;
@@ -39,6 +65,73 @@ const capProbCalc = (shakeProb, modCatchRate) => {
     return 100;
 
   return (Math.pow( (shakeProb/65535), 4) * 100);
+}
+
+const bestBallsCalc = (pkmn, gFlag) => {
+  let maxhp0 = baseStatCalc(pkmn.HP, 0, 30);
+  let maxhp31 = baseStatCalc(pkmn.HP, 31, 70);
+
+  let bestBalls = balls.filter(x => {
+    let notMB = x.name != "Master Ball";
+    let notQB = x.name != "Quick Ball";
+    return x.modifier >= 3 && notMB && notQB;
+  });
+  bestBalls.sort((x, y) => y.modifier > x.modifier);
+
+  let messageToSend = gFlag? `The best Poké-Balls for catching G-Max ${pkmn.Name} are:` : `The best Poké-Balls for catching ${pkmn.Name} are:`;
+
+  bestBalls.forEach(ball => {
+    let pkmnCatchRate = gFlag? 3 : pkmn.CatchRate;
+    let modCatchRate0 = CatchRateCalc(maxhp0, 1, pkmnCatchRate, ball.modifier);
+
+    let modCatchRate31 = CatchRateCalc(maxhp31, 1, pkmnCatchRate, ball.modifier);
+
+    let shakeProb0 = shakeProbCalc(modCatchRate0);
+    let shakeProb31 = shakeProbCalc(modCatchRate31);
+
+    catchProb0 = capProbCalc(shakeProb0, modCatchRate0).toFixed(2);
+
+    catchProb31 = capProbCalc(shakeProb31, modCatchRate31).toFixed(2);
+
+    ball.catchProb = `${catchProb0}% ~ ${catchProb31}%`;
+
+    messageToSend = messageToSend.concat(`\n${ball.name}: ${ball.catchProb}`);
+  });
+
+  return messageToSend;
+}
+
+const bestBallCalc = (pkmn, ball, gFlag) => {
+  let maxhp0 = baseStatCalc(pkmn.HP, 0, 30);
+  let maxhp31 = baseStatCalc(pkmn.HP, 31, 70);
+
+  let pkmnCatchRate = gFlag? 3 : pkmn.CatchRate;
+
+  let modCatchRate0 = CatchRateCalc(maxhp0, 1, pkmnCatchRate, ball.modifier);
+  let modCatchRate31 = CatchRateCalc(maxhp31, 1, pkmnCatchRate, ball.modifier);
+
+  let shakeProb0 = shakeProbCalc(modCatchRate0);
+  let shakeProb31 = shakeProbCalc(modCatchRate31);
+
+  catchProb0 = capProbCalc(shakeProb0, modCatchRate0).toFixed(2);
+  catchProb31 = capProbCalc(shakeProb31, modCatchRate31).toFixed(2);
+
+  ball.catchProb = `${catchProb0}% ~ ${catchProb31}%`;
+
+  let messageToSend = gFlag? `The probability of catching G-Max ${pkmn.Name} with a ${ball.name} is: ${ball.catchProb}` : `The probability of catching ${pkmn.Name} with a ${ball.name} is: ${ball.catchProb}` 
+
+  return messageToSend;
+}
+
+const ballFinder = (ballName) => {
+  return balls.find(ball => {
+    if (ball.name == ballName)
+      return true;
+
+    let a = ball.name.toLowerCase().replace(" ball", "");
+    let b = ballName.replace("ball", "");
+    return a == b;
+  });
 }
 
 client.on("ready", () => { console.log("I am ready!"); });
@@ -99,76 +192,121 @@ client.on("message", (message) => {
       let pkmn = ingamePkmn.find(x => {return x.Name.toLowerCase() == args[0].toLowerCase()});
       if (pkmn == null)
       {
-        message.channel.send("This Pokémon is not available in current dens.");
+        message.channel.send(pkmnNotFound);
         return;
       }
 
-      let maxhp0 = baseStatCalc(pkmn.HP, 0, 30);
-      let maxhp31 = baseStatCalc(pkmn.HP, 31, 70);
-
-      let bestBalls = balls.filter(x => {return x.modifier >= 3.5});
-      bestBalls.sort((x, y) => y.modifier > x.modifier);
-
-      let messageToSend = `The top 8 balls for catching ${pkmn.Name} are:`
-
-      bestBalls.forEach(ball => {
-        let modCatchRate0 = CatchRateCalc(maxhp0, 1, pkmn.CatchRate, ball.modifier);
-
-        let modCatchRate31 = CatchRateCalc(maxhp31, 1, pkmn.CatchRate, ball.modifier);
-
-        let shakeProb0 = shakeProbCalc(modCatchRate0);
-        let shakeProb31 = shakeProbCalc(modCatchRate31);
-
-        catchProb0 = capProbCalc(shakeProb0, modCatchRate0).toFixed(2);
-
-        catchProb31 = capProbCalc(shakeProb31, modCatchRate31).toFixed(2);
-
-        ball.catchProb = `${catchProb0}% ~ ${catchProb31}%`;
-
-        messageToSend = messageToSend.concat(`\n${ball.name}: ${ball.catchProb}`);
-
-        /*
-        console.log(`Everything:\nBall: ${ball.name}\nmodCatchRate0: ${modCatchRate0}\nmodCatchRate31: ${modCatchRate31}\nshakeProb0: ${shakeProb0}\nshakeProb31: ${shakeProb31}\n`);
-        */
-      });
+      let messageToSend = bestBallsCalc(pkmn, false);
 
       message.channel.send(messageToSend);
-
-      /*
-      let ballModifier = balls.find(x => x.name == "Poke Ball").modifier;
-      console.log(`Ball Modifier for Pokeball is: ${ballModifier}`);
-
-      let maxhp0 = baseStatCalc(pkmn.HP, 0, 30);
-      console.log(`Max HP at iv 0, level 30 is: ${maxhp0}`);
-      
-      let maxhp31 = baseStatCalc(pkmn.HP, 31, 70);
-
-      let mCR = modCatchRate(maxhp0, pkmn.CatchRate, ballModifier);
-
-      let sp = shakeProb(mCR);
-
-      console.log(`Mod Catch Rate for pkmn with max hp: ${maxhp0}, catch rate: ${pkmn.CatchRate}, and ball modifier: ${ballModifier}, is ${mCR}, with shake prob: ${sp}`);
-
-
-      message.channel.send(`Percent chance to catch ${pkmn.Name} with a pokeball is ${capProb(sp, mCR)}%`);
-      console.log(pkmn);
-      */
-    }
-  }
-
-  else if (args.length == 2)
-  {
-    let pkmn = ingamePkmn.find(x => {return x.Name.toLowerCase() == args[0].toLowerCase()});
-
-    //let ball = balls.find(x => ) rethink this.
-    if (pkmn == null)
-    {
-      message.channel.send("This Pokémon is not available in current dens.");
       return;
+
     }
 
+    else if (args.length == 2)
+    {
+      let possiblePkmnName = (args[0] + " " + args[1]).toLowerCase();
+      let possiblePkmn = ingamePkmn.find(x => {
+        return x.Name.toLowerCase() == possiblePkmnName
+      });
 
+      let ballName = ballNames.find(name => {
+        return name.toLowerCase() == args[1].toLowerCase();
+      });
+
+      if (gigaKeywords.includes(args[0].toLowerCase()))
+      {
+        let pkmn = ingamePkmn.find(x => {return x.Name.toLowerCase() == args[1].toLowerCase()});
+        if (pkmn == null)
+        {
+          message.channel.send(pkmnNotFound);
+          return;
+        }
+  
+        let messageToSend = bestBallsCalc(pkmn, true);
+  
+        message.channel.send(messageToSend);
+        return;
+      }
+
+      else if (possiblePkmn != null)
+      {
+        let messageToSend = bestBallsCalc(possiblePkmn, false);
+        message.channel.send(messageToSend);
+        return;
+      }
+
+      else if (ballName != null)
+      {
+        let pkmn = ingamePkmn.find(x => {
+          return x.Name.toLowerCase() == args[0].toLowerCase();
+        });
+
+        let ball = ballFinder(ballName);
+
+        if (pkmn == null)
+        {
+          message.channel.send(pkmnNotFound);
+          return;
+        }
+
+        let messageToSend = bestBallCalc(pkmn, ball, false);
+
+        message.channel.send(messageToSend);
+      }
+
+      else
+      {
+        message.channel.send(notFound);
+      }
+    }
+
+    else if (args.length == 3)
+    {
+
+      if (gigaKeywords.includes(args[0].toLowerCase()))
+      {
+        let possiblePkmnName = (args[1] + " " + args[2]).toLowerCase();
+        let possiblePkmn = ingamePkmn.find(x => {
+          return x.Name.toLowerCase() == possiblePkmnName
+        });
+        let pkmn = ingamePkmn.find(x => {
+          return x.Name.toLowerCase() == args[1].toLowerCase();
+        });
+
+        if (pkmn == null && possiblePkmn == null)
+        {
+          message.channel.send(pkmnNotFound);
+          return;
+        }
+
+        else if (pkmn)
+        {
+          let ballName = ballNames.find(name => {
+            return name.toLowerCase() == args[2].toLowerCase();
+          });
+
+          if (ballName == null)
+          {
+            message.channel.send(ballNotFound);
+            return;
+          }
+
+          let ball = ballFinder(ballName);
+
+          let messageToSend = bestBallCalc(pkmn, ball, true);
+
+          message.channel.send(messageToSend);
+        }
+
+        else
+        {
+          let messageToSend = bestBallsCalc(possiblePkmn, true);
+          message.channel.send(messageToSend);
+          return;
+        }
+      }
+    }
   }
 });
-
 client.login(config.token);
