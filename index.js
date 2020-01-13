bdc8635e45403c9efa9bf61424d8fb0ddd226a75
Raphael Guzman <raphael@frasif.com>
2020-01-13 16:42:08 -0500

    Index File

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const fs = require("fs");
+
+const client = new Discord.Client();
+client.config = config;
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.log(err);
+  
+  files.forEach(file => {
+    if (!file.endsWith("js")) return;
+
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Map();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.log(err);
+
+  files.forEach(file => {
+    if (!file.endsWith("js")) return;
+
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command: ${commandName}`);
+    client.commands.set(commandName, props);
+  });
+});
