c05a7adaf12c39eae44cbedeacca4a3352367ca8
Raphael <raphg@knights.ucf.edu>
2020-01-15 13:15:22 -0500

    Quick Edits

---
+++ commands/reload.js
@@ -0,0 +1,38 @@
+const botspeech = require("../modules/botspeech.js");
+
+exports.run = (client, message, args) => {
+  if (!args || args.length < 1)
+    return message.reply(botspeech.reloadNoArg);
+
+  const commandName = args[0];
+
+  if (!client.commands.has(commandName))
+    return message.reply(botspeech.commandNotFound);
+
+  else
+  {
+    delete require.cache[require.resolve(`./${commandName}.js`)];
+    client.commands.delete(commandName);
+    const props = require(`./${commandName}.js`);
+    client.commands.set(commandName, props);
+    message.reply(`The command ${commandName} has been reloaded.`);
+  }
+}
+
+/*
+exports.run = (client, message, args) => {
+  if(!args || args.length < 1) return message.reply("Must provide a command name to reload.");
+  const commandName = args[0];
+  // Check if the command exists and is valid
+  if(!client.commands.has(commandName)) {
+    return message.reply("That command does not exist");
+  }
+  // the path is relative to the *current folder*, so just ./filename.js
+  delete require.cache[require.resolve(`./${commandName}.js`)];
+  // We also need to delete and reload the command from the client.commands Enmap
+  client.commands.delete(commandName);
+  const props = require(`./${commandName}.js`);
+  client.commands.set(commandName, props);
+  message.reply(`The command ${commandName} has been reloaded`);
+};
+*/
