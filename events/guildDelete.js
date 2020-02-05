c5070227d1c54a0defe39a3a4fda3103c178bfec
Raphael <raphg@knights.ucf.edu>
2020-02-04 19:44:59 -0500

    GH-Pages Setup

--- events/guildDelete.js
+++
@@ -1,7 +0,0 @@
-const botspeech = require("../modules/botspeech.js");
-
-module.exports = (client) => {
-  client.user.setActivity(botspeech.presence.replace(/{{guildcount}}/, client.guilds.size));
-  
-  return console.log("Left Guild.");
-}
--- events/ready.js
+++
@@ -1,8 +0,0 @@
-const botspeech = require("../modules/botspeech.js");
-
-module.exports = (client) => {
-
-  client.user.setActivity(botspeech.presence.replace(/{{guildcount}}/, client.guilds.size));
-  
-  return console.log("I am ready!");
-};
