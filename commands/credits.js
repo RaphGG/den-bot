6e19cf84e25cfa26bc19e4640abb2659d9c50f76
Raphael <raphg@knights.ucf.edu>
2020-02-15 13:46:35 -0500

    Removal of Credits for Time Being

--- commands/credits.js
+++ commands/credits.js
@@ -1,6 +1,6 @@
 const embedHelper = require("../modules/embedHelper.js");
 
 exports.run = (client, message, args) => {
-  let embed = embedHelper.createEmbed("credits", client);
-  return message.channel.send(embed);
+  //let embed = embedHelper.createEmbed("credits", client);
+  //return message.channel.send(embed);
 }
