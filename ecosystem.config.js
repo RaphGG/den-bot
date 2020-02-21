70459dd73d0ef3a6e31b933dfc2b0d1a355b2427
Raphael <raphg@knights.ucf.edu>
2020-02-20 22:59:58 -0500

    Eco Edit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ exports = {
   apps: [{
     name: "Alcremie-B",
     script: "./index.js",
+    cwd: "/home/pi/DiscordBots/Den-Bot/",
     error_file: "./data/logs/error.log",
     out_file: "./data/logs/out.log",
     log_file: "./data/logs/combined.log",
