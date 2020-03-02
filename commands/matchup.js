95660451bca5a3e3652179dc3db65fb39d1a5e1d
Raphael Guzman <raphael@frasif.com>
2020-03-02 17:00:35 -0500

    Prelims for Matchup Command

---
+++ commands/matchup.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "Pokèmon Type Match-Up Command",
+  cmdName: "matchup",
+  aliases: ["type", "types", "matchups"],
+  description: "Shows a detailed summary of type matchups for the given type.",
+  args: 1,
+  usage: "{{prefix}}mathcup [Pokémon Type 1] (Pokémon Type 2)",
+  example: "{{prefix}}matchup fire flying",
+  guildOnly: false,
+  adminOnly: false,
+  run(client, message, args, settings) {
+    run(client, message, args, settings);
+  }
+};
+
+const run = (client, message, args, settings) => {
+  
+};
